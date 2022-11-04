d341ecbb506f766be122278057210bdd00893323
leo-demetrio <leocdemetrio@gamil.com>
2022-11-04 16:01:25 -0300

    Feat: Configurações e classe Produto

---
+++ src/main/java/com/leodemetrio/hibernatejpa/modelo/Produto.java
@@ -0,0 +1,16 @@
+package com.leodemetrio.hibernatejpa.modelo;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+}
