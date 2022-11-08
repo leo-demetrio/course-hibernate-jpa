c3dfc3a80e10b0a3c55498a1d55889c4f77b2502
leo-demetrio <leocdemetrio@gamil.com>
2022-11-08 20:14:36 -0300

    Feat: Querys

--- src/main/java/com/leodemetrio/hibernatejpa/dao/ProdutoDao.java
+++ src/main/java/com/leodemetrio/hibernatejpa/dao/ProdutoDao.java
@@ -3,6 +3,8 @@ package com.leodemetrio.hibernatejpa.dao;
 import com.leodemetrio.hibernatejpa.modelo.Produto;
 
 import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+import java.util.List;
 
 public class ProdutoDao {
 
@@ -14,5 +16,33 @@ public class ProdutoDao {
 
     public void cadastrar(Produto produto){
         this.entityManager.persist(produto);
+    }
+    public Produto buscarPorId(Long id){
+        return entityManager.find(Produto.class, id);
+    }
+    public List<Produto> buscarTodos(){
+        String queryJPQL = "SELECT p FROM Produto as p";
+        return  entityManager.createQuery(queryJPQL, Produto.class).getResultList();
+    }
+    public List<Produto> buscarPorNome(String nome){
+        String queryJPQL = "SELECT p FROM Produto as p WHERE p.nome = :nome";
+        return  entityManager
+                .createQuery(queryJPQL, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+    public List<Produto> buscarPorNomeDaCategoria(String nome){
+        String queryJPQL = "SELECT p FROM Produto as p WHERE p.categoria.nome = :nome";
+        return  entityManager
+                .createQuery(queryJPQL, Produto.class)
+                .setParameter("nome", nome)
+                .getResultList();
+    }
+    public BigDecimal buscarPrecoPorNome(String nome){
+        String queryJPQL = "SELECT p.preco FROM Produto as p WHERE p.nome = :nome";
+        return  entityManager
+                .createQuery(queryJPQL, BigDecimal.class)
+                .setParameter("nome", nome)
+                .getSingleResult();
     }
 }
--- src/main/java/com/leodemetrio/hibernatejpa/testes/CadastroProduto.java
+++ src/main/java/com/leodemetrio/hibernatejpa/testes/CadastroProduto.java
@@ -10,9 +10,29 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class CadastroProduto {
     public static void main(String[] args) {
+        cadastroProduto();
+        Long id = 1L;
+        EntityManager entityManager = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(entityManager);
+        Produto p = produtoDao.buscarPorId(id);
+        System.out.println(p.getDescricao());
+        List<Produto> ps = produtoDao.buscarTodos();
+        ps.forEach(pr -> System.out.println(pr.getNome()));
+        List<Produto> porNome = produtoDao.buscarPorNome("Sansung");
+        porNome.forEach(pn -> System.out.println(pn.getDescricao() + "*********************************"));
+        List<Produto> porNomeDaCategoria = produtoDao.buscarPorNomeDaCategoria("celulares");
+        System.out.println("=====================================");
+        System.out.println(porNomeDaCategoria);
+        porNomeDaCategoria.forEach(pnc -> System.out.println(pnc.getDescricao() + "*********************************"));
+        BigDecimal b = produtoDao.buscarPrecoPorNome("Sansung");
+        System.out.println(b);
+    }
+
+    private static void cadastroProduto() {
         Categoria cel = new Categoria("Celular");
         Produto celular = new Produto("Sansung","Celular Top",new BigDecimal("900.00"), cel);
         celular.setNome("Sansung");
